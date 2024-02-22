68568f22dc8059f0a3fb7bc01d2ebd7bf5c6fce1
natdm02 <natalidimarino2002>
2024-02-22 20:21:55 +0100

    [add]completo

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,20 @@
-// 1. creo un ciclo che stampi i numeri da 1 a 100
+console.log('fizzbuzz');
 
-for (var i = 1; i <= 100; i++) {
-// 2. multipli di 3 e di 5
-    if ((i % 3 == 0) && (i % 5 == 0)) {
-      
-// stampo 
+// container 
+const box = document.getElementById('mine-container');
+// ciclo da 1 a 100 
+for(let i = 1; i <= 100; i++){
 
-    console.log("FizzBuzz");
-    document.getElementById('listaNumeri').innerHTML += "<li>" + "FizzBuzz" + "</li>";
+  if(!(i % 3) && !(i % 5)){
+    box.innerHTML += `<div class="box violet green">fizz-buzz</div>`
+  }else if (!(i % 3)){
+    box.innerHTML += `<div class="box violet">fizz</div>`
+  }else if (!(i % 5)){
+    box.innerHTML += `<div class="box green">buzz</div>`
+  }else{
+    box.innerHTML += `<div class="box">${i}</div>`
+  }
 
-// multipli di 3    
-    }else if (i % 3 == 0) {
-      
-// stampo 
 
-    console.log("Fizz");
-    document.getElementById('listaNumeri').innerHTML += "<li>" + "Fizz" + "</li>";
-
-// multipli di 5
-    }else if (i % 5 == 0){
-      
-// stampo 
-    console.log("Buzz");
-    document.getElementById('listaNumeri').innerHTML += "<li>" + "Buzz" + "</li>";
-
-// stampo il numero
-    }else {
-
-// stampo
-    console.log(i);
-    document.getElementById('listaNumeri').innerHTML += "<li>" + [i] + "</li>";
-
-    }
+  console.log(box);
 }
