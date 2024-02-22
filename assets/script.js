532eae3d6ab43791240c441e0a6a42a9573f50a7
natdm02 <natalidimarino2002>
2024-02-22 15:59:25 +0100

    [add] multipli di 3/5

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,33 @@
 for (var i = 1; i <= 100; i++) {
 // 2. multipli di 3 e di 5
     if ((i % 3 == 0) && (i % 5 == 0)) {
-    
+      
+// stampo 
+
+    console.log("FizzBuzz");
+    document.getElementById('listaNumeri').innerHTML += "<li>" + "FizzBuzz" + "</li>";
+
+// multipli di 3    
+    }else if (i % 3 == 0) {
+      
+// stampo 
+
+    console.log("Fizz");
+    document.getElementById('listaNumeri').innerHTML += "<li>" + "Fizz" + "</li>";
+
+// multipli di 5
+    }else if (i % 5 == 0){
+      
+// stampo 
+    console.log("Buzz");
+    document.getElementById('listaNumeri').innerHTML += "<li>" + "Buzz" + "</li>";
+
+// stampo il numero
+    }else {
+
+// stampo
+    console.log(i);
+    document.getElementById('listaNumeri').innerHTML += "<li>" + [i] + "</li>";
+
     }
 }
